1fd0a1c4890dac9f23c04a175c47747891f31203
codewithria89 <anjalik0822@gmail.com>
2026-05-21 15:32:55 +0530

    New function files

---
+++ JavaScript/fun.js
@@ -0,0 +1,35 @@
+function add() {
+    let c = 10 + 20;
+    console.log(c);
+}
+
+add();
+
+//find square of number
+
+function square() {
+    let num = 2;
+    let sq = num*num; 
+    console.log(sq);
+}
+
+square();
+
+//find area of circle
+function area() {
+    let pi = 3.14;
+    let r = 3;
+    let ar = pi*r*r;
+    console.log(ar);
+}
+area();
+
+//findd simple interest --> ptr/100
+function SI() {
+    let p = 23;
+    let t = 3;
+    let r = 3;
+    let int = p*t*r/100;
+    console.log(int);
+}    
+SI();
