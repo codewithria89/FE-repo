ea868b708b1865f251f423ef88fb43e9983559cb
codewithria89 <anjalik0822@gmail.com>
2026-05-26 15:16:58 +0530

    New files

---
+++ JavaScript/hosting.js
@@ -0,0 +1,15 @@
+console.log(a);
+var a = 10;
+
+function greet() {
+    console.log("Hello good Evening");
+}
+greet();
+
+function add(a, b) {
+    console.log(a + b);
+}
+add(10,20);
+
+let b = 3.14;
+console.log(b);
