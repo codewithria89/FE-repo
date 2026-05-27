8466683251de613665500b9c02fc1aab3f3a76af
codewithria89 <anjalik0822@gmail.com>
2026-05-27 14:27:59 +0530

    Array file with example

---
+++ JavaScript/Array/arrays.js
@@ -0,0 +1,9 @@
+// Array 2 ways
+// Arrays in javaScript is Heterogeneous
+
+let stu_info = [102, "Abhi",8.9, true]
+console.log(stu_info)
+console.log(stu_info[1])
+//Array constructor 
+let arr = new Array(1, 2, 3, 4)
+console.log(arr)
